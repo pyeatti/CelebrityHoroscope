f4630c2a3ec9cf738c266ef24e7207782525f6cd
Evan <evanmohammedsaleh@gmail.com>
2020-08-05 11:49:27 -0500

    fixed bugs

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
 let array = [];
-<<<<<<< HEAD
-$("#search-button").on("click", function (event) {
-    let currentPerson = {};
-    event.preventDefault()
-    let $searchPerson = $("#search-text").val();
-    let queryURL = "http://api.themoviedb.org/3/search/person?query=" + $searchPerson + "&api_key=8ab67037c3d435e037521d6507495432";
-
-
-
-    console.log(event)
-
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        // console.log(response);
-        // console.log(response.results[0].id);
-        $("#results").text(response.results[0].name)
-    
-        currentPerson.name= response.results[0].name
-        // console.log(response.results[0].profile_path);
-        let imgURL = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + response.results[0].profile_path;
-        var img = $("<img>").attr("src", imgURL);
-        $(".container").append(img);
-        currentPerson.image = imgURL;
-
-
-
-
-        let personId = response.results[0].id;
-        currentPerson.id= personId
-        let birthDayURL = "https://api.themoviedb.org/3/person/" + personId + "?api_key=8ab67037c3d435e037521d6507495432";
-
-        return $.ajax({
-            url: birthDayURL,
-            method: "GET"
-        })
-=======
 $("#searchBtn").on("click", function (event) {
   $(".card").css("visibility", "visible");
   let currentPerson = {};
@@ -99,7 +60,6 @@ $("#searchBtn").on("click", function (event) {
       };
 
       return $.ajax(settings);
->>>>>>> f9c46e29972911de96858f744791cae94fb20ed3
     })
 
     .then(function (response) {
