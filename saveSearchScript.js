caf136d4b56f36c811b1cf4bce5ac9c4da149bf3
Evan <evanmohammedsaleh@gmail.com>
2020-08-07 09:20:33 -0500

    newtable

--- saveSearchScript.js
+++ saveSearchScript.js
@@ -7,10 +7,10 @@ let saveNames = actors.map( actor => actor.name);
 console.log(saveNames)
 for (let i = 0; i < saveNames.length; i++) {
     const actor= actors[i];
-    $("#savedSearch").append(`<ul class="list-group"><li class="loadActor list-group-item" data-sequence="${i}">${actor.name}</li></ul>`);
+    $("#savedSearch").append(`<ul class="list-group"><li class="list-group-item list-group-item-action list-group-item-light" data-sequence="${i}">${actor.name}</li></ul>`);
     
 }
-$(".loadActor").on("click" , function (event) {
+$(".list-group-item").on("click" , function (event) {
     $(".card").css("visibility", "visible");
     let currentPerson = actors[this.getAttribute("data-sequence")];
     console.log(currentPerson)
