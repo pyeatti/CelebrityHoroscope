8f42af921fde24c6ea5a636ff81310277b5c986d
Evan <evanmohammedsaleh@gmail.com>
2020-08-07 08:53:23 -0500

    added clear btn

--- saveSearchScript.js
+++ saveSearchScript.js
@@ -1,55 +1,61 @@
 let actors = JSON.parse(localStorage.getItem("actors"));
 if (actors === null) {
-    actors = [];
-} 
+  actors = [];
+}
 
-let saveNames = actors.map( actor => actor.name);
+let saveNames = actors.map(actor => actor.name);
 console.log(saveNames)
 for (let i = 0; i < saveNames.length; i++) {
-    const actor= actors[i];
-    $("#savedSearch").append(`<ul class="list-group"><li class="loadActor list-group-item" data-sequence="${i}">${actor.name}</li></ul>`);
-    
+  const actor = actors[i];
+  $("#savedSearch").append(`<ul class="list-group"><li class="list-group-item list-group-item-action list-group-item-light" data-sequence="${i}">${actor.name}</li></ul>`);
+
 }
-$(".loadActor").on("click" , function (event) {
-    $(".card").css("visibility", "visible");
-    let currentPerson = actors[this.getAttribute("data-sequence")];
-    console.log(currentPerson)
-        console.log(this.getAttribute("data-sequence"));
-        render(currentPerson);
-        console.log(currentPerson)
-    })
+$(".list-group-item").on("click", function (event) {
+  $(".card").css("visibility", "visible");
+  let currentPerson = actors[this.getAttribute("data-sequence")];
+  console.log(currentPerson)
+  console.log(this.getAttribute("data-sequence"));
+  render(currentPerson);
+  console.log(currentPerson)
+})
 
-      
-  function findSignImg(sign) {
 
-    switch (sign) {
-      case "Aries":
-        return "./signImages/aries.png" 
-      case "Taurus":
-        return "./signImages/taurus.png" 
-      case "Gemini":
-        return "./signImages/gemini.png" 
-      case "Cancer":
-        return "./signImages/cancer.png" 
-      case "Leo":
-        return "./signImages/leo.png" 
-      case "Virgo":
-        return "./signImages/virgo.png" 
-      case "Libra":
-        return "./signImages/libra.png" 
-      case "Scorpio":
-        return "./signImages/scorpio.png" 
-      case "Sagittarius":
-        return "./signImages/sagittarius.png" 
-      case "Capricorn":
-        return "./signImages/capricorn.png" 
-      case "Aquarius":
-        return "./signImages/aquarius.png" 
-      case "Pisces":
-        return "./signImages/pisces.png" 
-        
- 
-      default:
-        break;
-    }
+function findSignImg(sign) {
+
+  switch (sign) {
+    case "Aries":
+      return "./signImages/aries.png"
+    case "Taurus":
+      return "./signImages/taurus.png"
+    case "Gemini":
+      return "./signImages/gemini.png"
+    case "Cancer":
+      return "./signImages/cancer.png"
+    case "Leo":
+      return "./signImages/leo.png"
+    case "Virgo":
+      return "./signImages/virgo.png"
+    case "Libra":
+      return "./signImages/libra.png"
+    case "Scorpio":
+      return "./signImages/scorpio.png"
+    case "Sagittarius":
+      return "./signImages/sagittarius.png"
+    case "Capricorn":
+      return "./signImages/capricorn.png"
+    case "Aquarius":
+      return "./signImages/aquarius.png"
+    case "Pisces":
+      return "./signImages/pisces.png"
+
+
+    default:
+      break;
   }
+}
+
+
+$("#clearSearch").on("click" , function (event) {
+  // console.log(event)
+  localStorage.removeItem("actors");
+})
--- script.js
+++ script.js
@@ -104,69 +104,64 @@ $("#searchBtn").on("click", function (event) {
       render(currentPerson);
       currentTargetPerson = currentPerson;
     });
-  });
- 
-  
-  function render(currentPerson) {
-    $(".card-title").text(currentPerson.name);
-    let img = $(".card-img").attr("src", currentPerson.imgURL);
-    $(".card-img").append(img);
-    $("#birthDate").text(`Date of Birth : ${currentPerson.birthday}`);
-    $("#signCompatible").text(` Compatible with: ${currentPerson.compatibility}`);
-    $(".card-text").text(`Daily Horoscope : ${currentPerson.description}`);
-    $(".signImg").attr("src" , findSignImg(currentPerson.sign))
-    $(".signCompatibleImg").attr("src" , findSignImg(currentPerson.compatibility))
-    
-  }
-  
-  
-  function findSignImg(sign) {
+});
 
-    switch (sign) {
-      case "Aries":
-        return "./signImages/aries.png" 
-      case "Taurus":
-        return "./signImages/taurus.png" 
-      case "Gemini":
-        return "./signImages/gemini.png" 
-      case "Cancer":
-        return "./signImages/cancer.png" 
-      case "Leo":
-        return "./signImages/leo.png" 
-      case "Virgo":
-        return "./signImages/virgo.png" 
-      case "Libra":
-        return "./signImages/libra.png" 
-      case "Scorpio":
-        return "./signImages/scorpio.png" 
-      case "Sagittarius":
-        return "./signImages/sagittarius.png" 
-      case "Capricorn":
-        return "./signImages/capricorn.png" 
-      case "Aquarius":
-        return "./signImages/aquarius.png" 
-      case "Pisces":
-        return "./signImages/pisces.png" 
-        
- 
-      default:
-        break;
-    }
+
+function render(currentPerson) {
+  $(".card-title").text(currentPerson.name);
+  let img = $(".card-img").attr("src", currentPerson.imgURL);
+  $(".card-img").append(img);
+  $("#birthDate").text(`Date of Birth : ${currentPerson.birthday}`);
+  $("#signCompatible").text(` Compatible with: ${currentPerson.compatibility}`);
+  $(".card-text").text(`Daily Horoscope : ${currentPerson.description}`);
+  $(".signImg").attr("src", findSignImg(currentPerson.sign))
+  $(".signCompatibleImg").attr("src", findSignImg(currentPerson.compatibility))
+
+}
+
+
+function findSignImg(sign) {
+
+  switch (sign) {
+    case "Aries":
+      return "./signImages/aries.png"
+    case "Taurus":
+      return "./signImages/taurus.png"
+    case "Gemini":
+      return "./signImages/gemini.png"
+    case "Cancer":
+      return "./signImages/cancer.png"
+    case "Leo":
+      return "./signImages/leo.png"
+    case "Virgo":
+      return "./signImages/virgo.png"
+    case "Libra":
+      return "./signImages/libra.png"
+    case "Scorpio":
+      return "./signImages/scorpio.png"
+    case "Sagittarius":
+      return "./signImages/sagittarius.png"
+    case "Capricorn":
+      return "./signImages/capricorn.png"
+    case "Aquarius":
+      return "./signImages/aquarius.png"
+    case "Pisces":
+      return "./signImages/pisces.png"
+
+
+    default:
+      break;
   }
-  
-  
-  
-  
-  
-  
-  
-  $("#saveBtn").on("click" , function (event) {
-    console.log(currentTargetPerson)
-    event.preventDefault();
-    if (!actors.some(actor => actor.name === currentTargetPerson.name)) 
+}
+
+
+$("#saveBtn").on("click", function (event) {
+  console.log(currentTargetPerson)
+  event.preventDefault();
+  if (!actors.some(actor => actor.name === currentTargetPerson.name))
     actors.push(currentTargetPerson);
-      localStorage.setItem("actors", JSON.stringify(actors));
-    
-    })
+  localStorage.setItem("actors", JSON.stringify(actors));
 
-  
+})
+
+
